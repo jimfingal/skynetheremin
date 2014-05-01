20838c3d67f6bcdeb07e1c3237ad8757192c518b
Jim Fingal <jim.fingal@gmail.com>
2014-05-01 09:44:25 -0700

    pretty colors

--- server/public/js/visualizer.js
+++ server/public/js/visualizer.js
@@ -6,25 +6,56 @@ define(['js/easing.js', 'lib/animationshim.js'], function(easing) {
   var canvas_width, canvas_height;
   var canvas, canvas_2d;
   var bin_count, bar_width, num_bins_displayed;
+  var colors;
+
+  var initializeColors = function() {
+
+      var c1 = 205, c2 = 147, c3 = 176;
+      var colors = [];
+      
+      colors.push([c1, c2, c2]);
+      colors.push([c1, c2, c3]);
+      colors.push([c1, c2, c1]);
+      colors.push([c3, c2, c1]);
+      colors.push([c2, c2, c1]);
+      colors.push([c2, c3, c1]);
+      colors.push([c2, c1, c1]);
+      colors.push([c2, c1, c3]);
+      colors.push([c2, c1, c2]);
+      colors.push([c3, c1, c2]);
+
+      colors = colors.reverse();
+
+      return colors;
+  };
+
+  var getFillStyle = function(percent) {
+    
+    var alpha = easing_functions.easeOutCubic(percent, 0, 1, 1);
+    var index = Math.floor(easing_functions.easeInCubic(percent, 0, 1, 1) * 10);
+    var color_set = colors[index];
+    var r = color_set[0], g = color_set[1], b = color_set[2];
+
+    var fill = 'rgba(' + r + ',' + g + ',' + b + ',' + alpha + ')';
+    return fill;
+  }
 
   var drawSpectrum = function() {
 
-      canvas_2d.clearRect(0, 0, canvas_width, canvas_height);
-      var frequency_domain = new Uint8Array(bin_count);
-      analyzer_node.getByteFrequencyData(frequency_domain);
+    canvas_2d.clearRect(0, 0, canvas_width, canvas_height);
+    var frequency_domain = new Uint8Array(bin_count);
+    analyzer_node.getByteFrequencyData(frequency_domain);
 
-      for (var i = 0; i < num_bins_displayed; i++) {
+    for (var i = 0; i < num_bins_displayed; i++) {
 
-        var percent = frequency_domain[i] / 256;
+      var percent = frequency_domain[i] / 256;
+      canvas_2d.fillStyle = getFillStyle(percent);
 
-        var alpha = easing_functions.easeInQuint(percent, 0, 1, 1);
-        canvas_2d.fillStyle = 'rgba(204,147,147,' + alpha + ')';
-
-        var bar_height = Math.round(canvas_height * percent);
-        var offset_y = Math.round(canvas_height - bar_height);
-        var offset_x = Math.round(i * bar_width);
-        canvas_2d.fillRect(offset_x, offset_y, bar_width, bar_height);
-      }
+      var bar_height = Math.round(canvas_height * percent);
+      var offset_y = Math.round(canvas_height - bar_height);
+      var offset_x = Math.round(i * bar_width);
+      canvas_2d.fillRect(offset_x, offset_y, bar_width, bar_height);
+    }
 
   };
 
@@ -40,8 +71,11 @@ define(['js/easing.js', 'lib/animationshim.js'], function(easing) {
     bin_count = analyzer_node.frequencyBinCount;
     num_bins_displayed = bin_count / 6;
 
+    colors = initializeColors();
+
     canvas = document.querySelector('canvas');
     canvas_2d = canvas.getContext('2d');
+
 
     var resizeCanvas = function() {
       canvas_width = window.innerWidth;
