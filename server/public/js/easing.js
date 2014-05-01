da84ccefb0b887b373fea27755760cea8aec60bd
Jim Fingal <jim.fingal@gmail.com>
2014-05-01 14:50:35 -0700

    some linting
    berg not worth the effort

--- server/public/js/easing.js
+++ server/public/js/easing.js
@@ -11,20 +11,20 @@ define(function() {
             return c * (t /= d) * t + b;
         },
         easeOutQuad: function(t, b, c, d) {
-            return -c * (t /= d) * (t-2) + b;
+            return -c * (t /= d) * (t - 2) + b;
         },
         easeInOutQuad: function(t, b, c, d) {
-            if ((t/=d/2) < 1) return c/2 * t * t + b;
-            return -c / 2 * ((--t) * (t-2) - 1) + b;
+            if ((t /= d / 2) < 1) return c / 2 * t * t + b;
+            return -c / 2 * ((--t) * (t - 2) - 1) + b;
         },
         easeInCubic: function(t, b, c, d) {
-            return c * (t/=d)*t*t + b;
+            return c * (t /= d) * t * t + b;
         },
         easeOutCubic: function(t, b, c, d) {
-            return c * ((t=t/d-1)*t*t + 1) + b;
+            return c * ((t = t / d - 1) * t * t + 1) + b;
         },
         easeInOutCubic: function(t, b, c, d) {
-            if ((t/=d/2) < 1) return c/2*t*t*t + b;
+            if ((t /= d / 2) < 1) return c / 2 * t * t * t + b;
             return c/2* ((t-=2)*t*t + 2) + b;
         },
         easeInQuart: function(t, b, c, d) {
@@ -44,23 +44,23 @@ define(function() {
             return c * ((t=t/d-1)*t*t*t*t + 1) + b;
         },
         easeInOutQuint: function(t, b, c, d) {
-            if ((t/=d/2) < 1) return c/2*t*t*t*t*t + b;
-            return c/2* ((t-=2)*t*t*t*t + 2) + b;
+            if ((t /= d / 2) < 1) return c / 2 * t * t * t * t * t + b;
+            return c / 2 * ((t -= 2) * t * t * t * t + 2) + b;
         },
         easeInSine: function(t, b, c, d) {
-            return -c * Math.cos(t/d * (Math.PI/2)) + c + b;
+            return -c * Math.cos(t / d * (Math.PI / 2)) + c + b;
         },
         easeOutSine: function(t, b, c, d) {
-            return c * Math.sin(t/d * (Math.PI/2)) + b;
+            return c * Math.sin(t / d * (Math.PI / 2)) + b;
         },
         easeInOutSine: function(t, b, c, d) {
-            return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
+            return -c / 2 * (Math.cos(Math.PI * t / d) - 1) + b;
         },
         easeInExpo: function(t, b, c, d) {
-            return (t==0) ? b : c * Math.pow(2, 10 * (t/d - 1)) + b;
+            return (t == 0) ? b : c * Math.pow(2, 10 * (t / d - 1)) + b;
         },
         easeOutExpo: function(t, b, c, d) {
-            return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
+            return (t == d) ? b + c : c * (-Math.pow(2, -10 * t / d) + 1) + b;
         }
     };
 
